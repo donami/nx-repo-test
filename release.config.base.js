d534937e432f4456fae5b6741fc22546539c1246
Markus Hederström <markus.hederstrom@teliacompany.com>
2023-09-18 22:21:47 +0200

    fix(donami-conversation): update ci

--- release.config.base.js
+++ release.config.base.js
@@ -1,4 +1,11 @@
 module.exports = {
   extends: 'semantic-release-npm-github-publish',
-  branches: ['main'],
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
 };
