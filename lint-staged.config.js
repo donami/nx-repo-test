5a2ba6c12f34d7ea8866d33d956d5919d2be512b
Markus Hederström <markus.hederstrom@teliacompany.com>
2023-09-18 21:30:06 +0200

    fix: inital commit

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "**/*.{ts,js,json,md,html,css,scss}": [
+    "nx affected --target lint --uncommited --fix true",
+    "nx affected --target test --uncommited",
+    // "nx format:write --uncommited",
+  ],
+  // "{packages,tools}/**/*.{ts,js,json,md,html,css,scss}": [
+  //   "nx affected --target lint --uncommited --fix true",
+  //   "nx affected --target test --uncommited",
+  //   "nx format:write --uncommited",
+  // ],
+};
